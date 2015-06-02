47dd22d9551040de2b5b9f5343162a89292eb118
Martin Hagstrom <marhag87@gmail.com>
2015-06-02 20:11:36 +0200

    Make script output a little prettier

--- travis.sh
+++ travis.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 source /etc/profile.d/rvm.sh
 
+echo "\$ git clone $3 git"
 git clone $3 git
+echo "\$ cd git"
 cd git
+echo "\$ git checkout $4"
 git checkout $4
-echo rvm use $1 --install --fuzzy
+
+echo "\$ rvm use $1 --install --fuzzy"
 rvm use $1 --install --fuzzy
 
+echo "\$ gem install bundler"
 gem install bundler
 
+echo "\$ export PUPPET_GEM_VERSION=\"~> $2\""
 export PUPPET_GEM_VERSION="~> $2"
-echo $PUPPET_GEM_VERSION
+echo "\$ export BUNDLE_GEMFILE=$PWD/Gemfile"
 export BUNDLE_GEMFILE=$PWD/Gemfile
-echo $BUNDLE_GEMFILE
 
+echo "\$ bundle install --jobs=3 --retry=3"
 bundle install --jobs=3 --retry=3
 
 command=$(cat .travis.yml | shyaml get-value script)
 
-echo $command
+echo "\$ $command"
 eval $command
