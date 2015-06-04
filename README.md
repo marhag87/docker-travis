485de999a1a031735b0dac0c36e5a9e2d30c0454
Martin Hagstrom <marhag87@gmail.com>
2015-06-04 16:25:59 +0200

    Add ability to use local folder

--- README.md
+++ README.md
@@ -8,6 +8,11 @@ Suggested commands:
 docker run --rm -t -i local-travis:latest <ruby version> <puppet version> <git repo> <git commit>
 ```
 
+Using local folder:
+```
+docker run --rm -v $(pwd):/mnt:ro -t -i travis:latest <ruby version> <puppet version> <git repo> <git commit>
+```
+
 For example:
 ```
 docker run --rm -t -i local-travis:latest 1.8.7 3.7.5 https://github.com/emahags/puppet-module-vmware.git origin/master
--- travis.sh
+++ travis.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 source /etc/profile.d/rvm.sh
 
-echo "\$ git clone $3 git"
-git clone $3 git
-echo "\$ cd git"
-cd git
-echo "\$ git checkout $4"
-git checkout $4
+if [[ -f /mnt/.travis.yml ]]; then
+  echo "Detected mounted repo"
+  echo "\$ cp -r /mnt git"
+  cp -r /mnt git
+  echo "\$ cd git"
+  cd git
+else
+  echo "\$ git clone $3 git"
+  git clone $3 git
+  echo "\$ cd git"
+  cd git
+  echo "\$ git checkout $4"
+  git checkout $4
+fi
 
 echo "\$ rvm use $1 --install --fuzzy"
 rvm use $1 --install --fuzzy
